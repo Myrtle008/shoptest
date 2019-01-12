fd49b073f5482426d0111c2a85f66102dfe3065e
vagrant <vagrant@homestead>
2019-01-12 03:49:07 +0000

    注册与登录

--- app/Http/Controllers/HomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-class HomeController extends Controller {
-	public function home() {
-		return view('index.home');
-	}
-}
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,8 @@
 |
  */
 
-Route::get('/', 'HomeController@home');
+Route::get('/', 'PagesController@root')->name('root');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
